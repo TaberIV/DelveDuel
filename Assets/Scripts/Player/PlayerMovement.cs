557730bd512276dbffd27dfac545f8f4a7b2e298
Taber <emcfarli@stevens.edu>
2018-11-05 11:07:27 -0500

    Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float MoveSpeed = 5;
+
+	private Transform trans;
+
+	void Awake()
+	{
+		trans = GetComponent<Transform>();
+	}
+
+	void Update()
+	{
+		Movement();
+	}
+
+	private void Movement()
+	{
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+		Vector2 pos = trans.position;
+		pos += input * MoveSpeed * Time.deltaTime;
+
+		trans.position = pos;
+	}
+}
