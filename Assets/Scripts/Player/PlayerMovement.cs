c4e99f63d00649d5ecdcf957a3335001a0382106
Taber McFarlin <tabermcfarlin@gmail.com>
2018-11-05 15:56:43 -0500

    Horizontal collisions

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -39,42 +39,98 @@ public class PlayerMovement : MonoBehaviour
 		Vector2 movement = velocity * Time.deltaTime;
 
 		// Horizontal Movement
-		transform.position += Vector3.right * movement.x;
+		if (movement.x != 0)
+		{
+			float dir = Mathf.Sign(movement.x);
+
+			// Origin of first vector
+			Vector2 origin = new Vector2(
+				(dir > 0 ? col.bounds.max.x : col.bounds.min.x) +
+				dir * RayMargin,
+				col.bounds.min.y
+			);
+
+			// Raycasting
+			bool collided = false;
+			float raySpacing = col.bounds.size.y / (NumRays - 1);
+			RaycastHit2D closestHit = new RaycastHit2D();
+			closestHit.distance = Mathf.Infinity;
+
+			for (int i = 0; i < NumRays; i++)
+			{
+				RaycastHit2D hitInfo = Physics2D.Raycast(origin, Vector2.right * dir, Mathf.Abs(movement.x));
+				Debug.DrawRay(origin, Vector2.right * dir, Color.red);
+				Debug.DrawRay(origin, Vector2.right * movement.x, Color.blue);
+
+				if (hitInfo.collider != null)
+				{
+					if (hitInfo.distance < closestHit.distance)
+					{
+						closestHit = hitInfo;
+					}
+
+					collided = true;
+				}
+
+				origin += Vector2.up * raySpacing;
+			}
+
+			if (!collided) // No collisions, move normally
+			{
+				transform.position += Vector3.right * movement.x;
+			}
+			else // Move as close to the wall as possible
+			{
+				float newX = closestHit.point.x - dir * (col.bounds.extents.x + RayMargin);
+				trans.position = new Vector3(newX, trans.position.y);
+			}
+		}
 
 		// Vertical Movement
 		if (movement.y != 0)
 		{
 			float dir = Mathf.Sign(movement.y);
 
+			// Origin of first vector
 			Vector2 origin = new Vector2(
 				col.bounds.min.x,
 				(dir > 0 ? col.bounds.max.y : col.bounds.min.y) +
 				dir * RayMargin
 			);
 
-			bool move = true;
+			// Raycasting
+			bool collided = false;
 			float raySpacing = col.bounds.size.x / (NumRays - 1);
+			RaycastHit2D closestHit = new RaycastHit2D();
+			closestHit.distance = Mathf.Infinity;
 
 			for (int i = 0; i < NumRays; i++)
 			{
 				RaycastHit2D hitInfo = Physics2D.Raycast(origin, Vector2.up * dir, Mathf.Abs(movement.y));
+				Debug.DrawRay(origin, Vector2.up * dir, Color.red);
+				Debug.DrawRay(origin, Vector2.up * movement.y, Color.blue);
 
 				if (hitInfo.collider != null)
 				{
-					move = false;
+					if (hitInfo.distance < closestHit.distance)
+					{
+						closestHit = hitInfo;
+					}
 
-					float newY = dir > 0 ? (hitInfo.point.y - col.bounds.extents.y) - RayMargin : hitInfo.point.y + col.bounds.extents.y + RayMargin;
-
-					trans.position = new Vector2(trans.position.x, newY);
-					break;
+					collided = true;
 				}
 
 				origin += Vector2.right * raySpacing;
 			}
 
-			if (move)
+			if (!collided) // No collisions, move normally
 			{
 				transform.position += Vector3.up * movement.y;
+			}
+			else // Move as close to the wall as possible
+			{
+				float newY = closestHit.point.y - dir * (col.bounds.extents.y + RayMargin);
+				trans.position = new Vector3(trans.position.x, newY);
 			}
 		}
 	}
