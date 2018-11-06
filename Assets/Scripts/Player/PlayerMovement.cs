fa55a9fb036879c4b93a6a1e27baf9b3150f068a
Taber <emcfarli@stevens.edu>
2018-11-06 00:18:56 -0500

    Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,8 +8,8 @@ public class PlayerMovement : MonoBehaviour
     // Inspector Settings
     // Movement Settings
     public float MoveSpeed = 5f;
-    public float MoveForce = 10f;
-    public float Friction = 20f;
+    public float MoveForce = 30f;
+    public float Friction = 50f;
 
     // Collision Settings
     public int NumRays = 3;
@@ -146,14 +146,78 @@ public class PlayerMovement : MonoBehaviour
     private void GetInput()
     {
         moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (moveInput.magnitude > 1)
+        {
+            moveInput.Normalize();
+        }
     }
 
     private void Movement()
     {
-        velocity = moveInput * MoveSpeed;
+        ApplyFriction();
+
+        Vector2 targetVelocity = GetTargetVelocity();
+        Acclerate(targetVelocity);
 
         Vector2 movement = velocity * Time.deltaTime;
-
         Move(movement);
+    }
+
+    private Vector2 GetTargetVelocity()
+    {
+        return moveInput * MoveSpeed;
+    }
+
+    private void ApplyFriction()
+    {
+        // Only apply friction on moving character
+        if (velocity.magnitude > 0)
+        {
+            float newSpeed = velocity.magnitude - Friction * Time.deltaTime;
+
+            // Makes sure friction doesn't send player backwards
+            if (newSpeed < 0)
+            {
+                newSpeed = 0;
+            }
+
+            // Scale velocity vector such that velocity.magnitude = newSpeed
+            float scale = newSpeed / velocity.magnitude;
+            velocity *= scale;
+        }
+    }
+
+    private void Acclerate(Vector2 targetVelocity)
+    {
+        Vector2 targetDirection = targetVelocity.normalized;
+        float targetSpeed = targetVelocity.magnitude;
+
+        // Gets current speed that is in the desired direction
+        float currentSpeed = Vector2.Dot(velocity, targetDirection);
+        float addSpeed = targetSpeed - currentSpeed;
+
+        // If player is slowing down or already going max
+        // speed in desired direction, do not accelerate.
+        if (addSpeed <= 0)
+        {
+            return;
+        }
+
+        float deltaSpeed = (MoveForce + Friction) * Time.deltaTime;
+
+        // Don't overshoot targetVelocity
+        if (deltaSpeed > addSpeed)
+        {
+            deltaSpeed = addSpeed;
+        }
+
+        velocity += deltaSpeed * targetDirection;
+
+        // TODO find a way to reduce move speed
+        if (velocity.magnitude > MoveSpeed)
+        {
+            velocity = velocity.normalized * MoveSpeed;
+        }
     }
 }
