4b02e77201f75eff3902ea2be9d7bc94d76b2f79
ngoldstein51 <ngoldstein51@gmail.com>
2018-11-09 17:43:50 -0500

    dammage sprites

--- Assets/Scripts/Player/PlayerDamageable.cs
+++ Assets/Scripts/Player/PlayerDamageable.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerDamageable : Damageable
 {
-	public override void Death()
+    public Sprite oneSlime;
+    public Sprite twoSlime;
+
+    public override void Death()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
+
+    public override void ReceiveDamage(float damage, GameObject damager = null)
+    {
+        base.ReceiveDamage(damage, damager);
+        Image currHealth = GameObject.Find("Health").GetComponent<Image>();
+
+        if(Health == 40)
+        {
+            currHealth.sprite = twoSlime;
+        }
+        else if(Health == 20)
+        {
+            currHealth.sprite = oneSlime;
+        }
+    }
 }
