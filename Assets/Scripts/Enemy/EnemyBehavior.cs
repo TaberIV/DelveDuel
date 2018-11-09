13328e55b7eaf1863d65026fbae2afc1e733e3e4
Taber <emcfarli@stevens.edu>
2018-11-09 14:11:39 -0500

    Enemy movement and health

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController2D))]
 public class EnemyBehavior : MonoBehaviour
 {
 	// Inpsector Settings
-	public float Speed = 3;
+	public float BurstSpeed = 3;
+	public float DriftSpeed = 1;
+	public float BurstTimeMax = 3;
+	public float BurstTimeMin = 1;
 
 	// Components
 	Transform trans;
+	CharacterController2D controller;
 
 	// Other objects
 	private Transform player;
 
+	// State
+	private Vector2 velocity;
+	private float burstTimer;
+	private bool burst;
+
 	void Awake()
 	{
+		// GetComponents
 		trans = GetComponent<Transform>();
+		controller = GetComponent<CharacterController2D>();
+
+		// Initialize State
+		velocity = new Vector2();
+
+		burst = Random.Range(0, 1) > 0.5;
+		burstTimer = Random.Range(BurstTimeMin, BurstTimeMax);
 	}
 
 	void Start()
@@ -25,6 +43,19 @@ public class EnemyBehavior : MonoBehaviour
 
 	void Update()
 	{
-		trans.position = Vector3.MoveTowards(trans.position, player.position, 3 * Time.deltaTime);
+		if (burstTimer > 0)
+		{
+			Vector3 diff = (player.position - trans.position).normalized;
+
+			float speed = burst ? BurstSpeed : DriftSpeed;
+			controller.Move(diff * speed * Time.deltaTime);
+
+			burstTimer -= Time.deltaTime;
+		}
+		else
+		{
+			burstTimer = Random.Range(BurstTimeMin, BurstTimeMax);
+			burst = !burst;
+		}
 	}
 }
---
+++ Assets/Scripts/Player/PlayerDamageable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDamageable : Damageable
+{
+	public override void Death()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
