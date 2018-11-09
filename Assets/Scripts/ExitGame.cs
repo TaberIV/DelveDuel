e2acea2ecfe2479a84ad5f70b444d013991163da
ngoldstein51 <ngoldstein51@gmail.com>
2018-11-09 18:38:48 -0500

    final

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Quit") > 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
