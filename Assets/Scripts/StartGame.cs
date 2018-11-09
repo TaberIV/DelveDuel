6b60ec97582d156abf7a7ffae04ca7d5afff1bbc
ngoldstein51 <ngoldstein51@gmail.com>
2018-11-09 16:56:52 -0500

    game start

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 input= new Vector2(Input.GetAxis("Start"), Input.GetAxis("Quit"));
+
+        if(input[1]>0)
+        {
+            Application.Quit();
+        }
+        else if (input[0]>0)
+        {
+            print("hi");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
